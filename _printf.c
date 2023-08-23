c3f543bbfedf5c647c4dca2c1a50fee17db0e0ce
santan dave <48166287+amaningobed@users.noreply.github.com>
2023-08-22 19:01:16 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
-	return (-1);
+		return (-1);
 
 	va_start(list, format);
 
@@ -25,7 +25,7 @@ int _printf(const char *format, ...)
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-			print_buffer(buffer, &buff_ind);
+				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
@@ -38,9 +38,9 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-			flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
-			return (-1);
+				return (-1);
 			printed_chars += printed;
 		}
 	}
@@ -60,7 +60,7 @@ int _printf(const char *format, ...)
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-	write(1, &buffer[0], *buff_ind);
+		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
 }
