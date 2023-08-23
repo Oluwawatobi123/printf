1854de08d44ce713b87334278811c67afd8b69d9
santan dave <48166287+amaningobed@users.noreply.github.com>
2023-08-22 19:44:00 -0500

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -4,7 +4,7 @@
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
- * @buffer: Bufferr array to handle print.
+ * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
